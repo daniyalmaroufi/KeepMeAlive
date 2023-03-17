b33793c1e63a7be01db1dc88c0ed3ac038333899
Daniyal Maroufi <daniyaleight8@gmail.com>
2023-03-17 23:22:48 +0330

    added unsplash api

--- keepmealive.php
+++ keepmealive.php
@@ -10,11 +10,26 @@ function sendMessage($chat_id, $text)
     file_get_contents($url);
 }
 
-function sendPhoto($chat_id, $photo)
+function sendPhoto($chat_id, $photo, $caption='')
 {
     global $bot_api;
     $request_url="https://api.telegram.org/bot".$bot_api."/";
-    $url = $request_url . 'sendPhoto?chat_id=' . $chat_id . '&photo=' . urlencode($photo);
+    $url=$request_url.'sendPhoto?chat_id='.$chat_id.'&photo='.urlencode($photo).'&caption='.urlencode($caption).'&parse_mode=Markdown';
+    echo $url;
     file_get_contents($url);
 }
+
+function getUnspalsh()
+{
+    global $unsplash_api;
+    $request_url='https://api.unsplash.com/photos/random?client_id='.$unsplash_api.'&orientation=landscape';
+    $result=json_decode(file_get_contents($request_url), true);
+    $unsplash=array();
+    $unsplash['photo']=$result['urls']['full'];
+    $unsplash['link']=$result['links']['html'];
+    $unsplash['name']=$result['user']['name'];
+    $unsplash['alt']=$result['alt_description'];
+    return $unsplash;
+}
+
 
